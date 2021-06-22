fdad10d9789e66f10c53b911dd24b99ca26072d7
Julio Junior <juniorknx@hotmail.com>
2021-06-22 00:18:02 -0300

    Ajustando margins componente card

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,37 @@
-import '../Home/style.css'
+import '../Home/style.css';
+import api from '../../services/api';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 export function Home (){
+
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(()=>{
+
+    async function loadFilmes(){
+        const response = await api.get('r-api/?api=filmes')
+        setFilmes(response.data)
+    }   
+    
+    loadFilmes();
+
+    },[]);
+
     return (
         <section id="movies">
-            <h1>To aqui HOME PAGE</h1>
+            <div className="movie-items">
+                {filmes.map((filme)=>{
+                    return (
+                        <div className="moviecard" key={filme.id}>
+                            <h2>{filme.nome}</h2>
+                            <img src={filme.foto} alt={filme.nome} />
+                            <p>{filme.sinopse}</p>
+                            <Link to="/">Acessar</Link>
+                        </div>
+                    );
+                })}
+            </div>
         </section>
     );
 }
