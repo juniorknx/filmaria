dba3924cde0ea3b96c907a068ea5433abc9780da
Julio Junior <juniorknx@hotmail.com>
2021-06-21 22:40:45 -0300

    Add routes

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,17 @@
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import { Contact } from './pages/Contato';
+import { Header } from './components/Header';
 
 import {Home} from './pages/Home'
 
 const Routes = () => {
     return (
         <BrowserRouter>
-            <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path="/contato" component={Contact} />
-            </Switch>
+            <Header />
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route path="/contato" component={Contact} />
+                </Switch>
         </BrowserRouter>
     )
 }
