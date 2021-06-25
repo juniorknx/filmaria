c16548bbea39d858a83aa3acd917f494c9775ecb
Julio Junior <juniorknx@hotmail.com>
2021-06-24 23:01:02 -0300

    Add 404 page on routes

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,8 @@ import { Header } from './components/Header';
 
 import {Home} from './pages/Home'
 import {Filme} from './pages/Filme'
+import {Favorites} from './pages/Favoritos'
+import { notFound } from './pages/404';
 
 const Routes = () => {
     return (
@@ -11,8 +13,9 @@ const Routes = () => {
             <Header />
                 <Switch>
                     <Route exact path="/" component={Home} />
-                    <Route path="/contato" component={Contact} />
+                    <Route exact path="/Favoritos" component={Favorites} />
                     <Route exact path="/filme/:id" component={Filme} />
+                    <Route path="*" component={notFound} />
                 </Switch>
         </BrowserRouter>
     )
